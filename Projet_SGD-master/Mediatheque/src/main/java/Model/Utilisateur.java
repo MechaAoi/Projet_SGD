3bcfd63256a12f3735580e5cf5af286fddd399ef
corentineuvrard <corentin.euvrard@gmail.com>
2018-12-15 20:55:13 +0100

    Update Utilisateur.java

--- Projet_SGD-master/Mediatheque/src/main/java/Model/Utilisateur.java
+++ Projet_SGD-master/Mediatheque/src/main/java/Model/Utilisateur.java
@@ -1,8 +1,5 @@
 package Model;
 
-//import java.util.Map;
-//import java.util.HashMap;
-
 import com.mongodb.DBObject;
 import java.util.List;
 import java.util.ArrayList;
@@ -13,7 +10,6 @@ public class Utilisateur
 	private String pseudo;
 	private String mdp;
 	private int admin;
-	//private List<Map<String, Object>> avis;
 	private List<Avis> avis;
 
 	public Utilisateur(DBObject obj)
@@ -24,10 +20,7 @@ public class Utilisateur
 		admin  = 1;
                 else
                 admin = 0;   
-		//avis   = new HashMap<String, Object>();
 
-		//ObjectMapper mapper = new ObjectMapper();
-		//avis = mapper.readValue(obj.get("avis"), new TypeReference<Map<String, String>>(){});
 		avis = new ArrayList<Avis>();
                 avis = (List<Avis>) obj.get("avis");
 	}
@@ -36,20 +29,25 @@ public class Utilisateur
 	public String getPseudo() { return pseudo; }
         public String getMdp() {return mdp;}
 	public int getAdmin() { return admin; }
-	//public Map<String, Object> getAvis() { return avis; }
 	public List<Avis> getAvis() { return avis ;}
 
 	// Setter
 	public void setPseudo(String _pseudo) { pseudo = _pseudo; }
 	public void setMdp(String _mdp) { mdp = _mdp; }
 	public void setAdmin(int _admin) { admin = _admin; }
-	//public void setAvis(Map<String, Object> _avis) { avis = _avis; }
 	public void setAvis(List<Avis> _avis) { avis = _avis; }
 
 	public void ajouterAvis(float note, int jeu, String commentaire)
 	{
-		//avis.put()
 		Avis nouvelAvis = new Avis(note, jeu, commentaire);
 		avis.add(nouvelAvis);
 	}
+	
+	public void modifierDroits(Utilisateur utilisateur)
+        {
+            if (this.admin == 1)
+            {
+                utilisateur.admin = 1;
+            }
+        }
 }
